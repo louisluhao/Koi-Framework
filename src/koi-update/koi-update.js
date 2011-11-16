d83a4c9f80e6ba4f18c9b41d9638afe62d9c0321
Eric Garside <eric@knewton.com>
2011-11-15 21:47:51 -0500

    Adding the getElements/hasClass methods.

--- src/koi-update/koi-update.js
+++ src/koi-update/koi-update.js
@@ -17,6 +17,16 @@
     //------------------------------
 
     //------------------------------
+    // Selection
+    //------------------------------
+
+        /**
+         * Matches strings which start with a pound.
+         * @type {RegExp}
+         */
+    var RX_ID = /^#/;
+
+    //------------------------------
     //
     // Properties
     //
@@ -29,6 +39,68 @@
     //------------------------------
 
     //------------------------------
+    // Selection
+    //------------------------------
+
+    /**
+     * Determines if the element has a class.
+     * @param {HTMLElement} element The element.
+     * @param {string} className The class.
+     * @return {boolean} True if the element has the class.
+     */
+    function hasClass(element, className) {
+        if (KOI.isValid(element.className)) {
+            return KOI.inArray(className, element.className.split(" "));
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Finds a set of elements using a class.
+     * @param {string} selector The selector. Either a class name or an #id.
+     * @param {string|HTMLElement=} context Context to work from. Can either be
+     *     an HTMLElement or an element id string.
+     * @param {number=} instance Instance selection.
+     * @return {?Array<HTMLElement>|HTMLElement} Matched elements.
+     */
+    function getElements(selector, context, instance) {
+        if (RX_ID.test(selector)) {
+            return document.getElementById(selector.replace(RX_ID, ""));
+        }
+        var elements,
+            classTest = new RegExp("(^|\\s)" + selector + "(\\s|$)"),
+            selected = [],
+            index = 0;
+        if (!KOI.isValid(context)) {
+            context = document;
+        } else {
+            if (KOI.isString(context)) {
+                context = document.getElementById(context); 
+            }
+            if (hasClass(context, selector)) {
+                selected.push(context);
+            }
+        }
+        elements = context.getElementsByTagName("*");
+        for (; index < elements.length; index++) {
+            if (classTest.test(elements[index].className)) {
+                selected.push(elements[index]); 
+            }
+        }
+        if (KOI.isValid(instance) && KOI.isNumber(instance)) {
+            return selected.length < instance ? null : selected[instance];
+        }
+        if (selected.length === 0) {
+            return null;
+        }
+        if (selected.length === 1) {
+            return selected[0];
+        }
+        return selected;
+    }
+
+    //------------------------------
     //
     // Event bindings
     //
@@ -39,6 +111,17 @@
     // Exposure
     //
     //------------------------------
+
+    KOI.expose({
+    
+    //------------------------------
+    // Selection
+    //------------------------------
+
+        hasClass: hasClass,
+        getElements: getElements
+
+    });
 
 }(window.KOI));
 
--- test/koi-update/test.js
+++ test/koi-update/test.js
@@ -11,6 +11,35 @@
     "use strict";
    
     Screw.Unit(function () {
+        describe("selection", function () {
+            it("can select elements from id", function () {
+                var e = KOI.getElements("#header-1");    
+                expect(e).to(have_id, "header-1");
+                expect(e).to(have_classes, "test-1");
+            });
+            it("can select elements from a class", function () {
+                expect(KOI.getElements("test-1")).to(have_length, 2);
+                expect(KOI.getElements("test-2")).to(have_id, "test-parent");
+                expect(KOI.getElements("test-3")).to(have_length, 3);
+            });
+            it("can select elements contextually", function () {
+                var e = KOI.getElements("#test-parent");
+                expect(KOI.getElements("test-3", e)).to(have_length, 2);
+                expect(KOI.getElements("test-3", e.id)).to(have_length, 2);
+                expect(KOI.getElements("test-2", e.id)).to(have_id, e.id);
+            });
+            it("can select element instances", function () {
+                expect(KOI.getElements("test-1", document, 1))
+                    .to(have_id, "header-2");
+                expect(KOI.getElements("test-1", document, 100)).to(be_null);
+                expect(KOI.getElements("test-4", "test-parent", 1).innerHTML)
+                    .to(equal, "c");
+            });
+            it("can test for classes", function () {
+                expect(KOI.hasClass(KOI.getElements("#test-parent"), "test-2"))
+                    .to(be_true);
+            });
+        });
     });
 
 }());
