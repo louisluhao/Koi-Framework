70232aff64138cdf6391d23b9a204bbb97c4b341
Eric Garside <eric@knewton.com>
2012-03-09 12:42:18 -0500

    Multiple route support for deeplink

--- test/deeplink/deeplink.js
+++ test/deeplink/deeplink.js
@@ -12,41 +12,106 @@
 
 	Screw.Unit(function () {
 		describe("deeplink event resolution", function () {
-
-			it("can execute events", function () {
+			it("handles routes", function () {
 				var triggered = false;
-
-				KOI.routes({
-					"/change": function () { triggered = true; },
+				KOI.route("/change/", function () {
+					triggered = true;
 				});
-
-				// cheating a bit, but it's not my fault hashchanges are async
-				KOI.executeRoute('/change');
+				KOI.executeRoute("/change");
 
 				expect(triggered).to(be_true);
 			});
-
-			it("can extract variables from a route", function () {
-				var total = 0, test_string = '';
-
-				KOI.routes({
-					"/add/:value/to/total": function () { total += parseInt(this.value, 10); },
-					"/set/string/to/:string": function() { test_string = this.string; }
+			it("supports multiple route listeners", function () {
+				var t1 = false,
+					t2 = false;
+				KOI.route("/a", function () {
+					t1 = true;
 				});
+				KOI.route("/a", function () {
+					t2 = true;
+				});
+				KOI.executeRoute("/a");
 
-				KOI.executeRoute('/add/5/to/total');
-				KOI.executeRoute('/add/10/to/total');
-				KOI.executeRoute('/set/string/to/worked');
+				expect(t1).to(be_true);
+				expect(t2).to(be_true);
+			});
+			it("is insentitive to trailing and leading slashes", function () {
+				var t1 = false,
+					t2 = false,
+					t3 = false;
+				KOI.route("/b", function () {
+					t1 = true;
+				});
+				KOI.route("b/", function () {
+					t2 = true;
+				});
+				KOI.route("/b/", function () {
+					t3 = true;
+				});
+				KOI.executeRoute("b");
+
+				expect(t1).to(be_true);
+				expect(t2).to(be_true);
+				expect(t3).to(be_true);
+			});
+			it("triggers errors on missing paths", function () {
+				var errored = false,
+					path = "/bad/path";
+				KOI.bind("deeplink-error", function (e, r) {
+					if (e === "not_found" && r === path) {
+						errored = true;
+					}
+				});
+				KOI.executeRoute(path);
+
+				expect(errored).to(be_true);
+			});
+			it("can extract variables from a route", function () {
+				var total = 0,
+					test_string;
+				KOI.routes({
+					"/add/:value/to/total": function (p) {
+						total += parseInt(p.value, 10);
+					},
+					"/set/string/to/:string": function(p) {
+						test_string = p.string;
+					}
+				});
+				KOI.executeRoute("/add/5/to/total");
+				KOI.executeRoute("/add/10/to/total");
+				KOI.executeRoute("/set/string/to/worked");
 
 				expect(total).to(equal, 15);
-				expect(test_string).to(equal, 'worked');
+				expect(test_string).to(equal, "worked");
 			});
+			it("can handle multiple variable matches", function () {
+				var t1 = 0,
+					t2 = 0,
+					last_action;
+				KOI.routes({
+					"/:action/:value/": function (p) {
+						var v = parseInt(p.value, 10);
+						if (p.action === "inc") {
+							t1 += v;
+						} else if (p.action === "dec") {
+							t1 -= v;
+						}
+						last_action = p.action;
+					},
+					"/inc/:value": function (p) {
+						t2 += parseInt(p.value, 10);
+					},
+					"/dec/:value": function (p) {
+						t2 -= parseInt(p.value, 10);
+					}
+				});
+				KOI.executeRoute("/inc/8");
+				KOI.executeRoute("/dec/3");
 
-			it("gracefully handles bad routes", function () {
-				KOI.executeRoute('/add/10/but/not/to/total');
-				KOI.executeRoute('/im/a/bad/route/');
+				expect(t1).to(equal, 5);
+				expect(t1).to(equal, t2);
+				expect(last_action).to(equal, "dec");
 			});
-
 		});
 	});
 }());
